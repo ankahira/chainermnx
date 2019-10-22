82a01130aa1048c7687523f832f5e5443343b717
ankahira <ankahira@gmail.com>
2019-10-23 02:28:12 +0900

    fix halo exchange

--- chainermnx/functions/__init__.py
+++ chainermnx/functions/__init__.py
@@ -1,2 +1,4 @@
 
 from chainermnx.functions.communication import allreduce
+from chainermnx.functions.concat import concat
+
--- chainermnx/links/spatial_parallel_convulution_2d.py
+++ chainermnx/links/spatial_parallel_convulution_2d.py
@@ -1,7 +1,8 @@
 import chainer
 import chainermn
 import chainer.functions as F
-
+from chainermnx.functions import concat
+import time
 
 class SpatialConvolution2D(chainer.links.Convolution2D):
     def __init__(self, comm, conv_index, in_channels, out_channels, *args, **kwargs):
@@ -23,31 +24,30 @@ class SpatialConvolution2D(chainer.links.Convolution2D):
         x = self.halo_exchange_forward(x)
         # x = self.halo_exchange_backward(x)
         y = super(SpatialConvolution2D, self).__call__(x)
-        return y
+        if self.comm.rank > 0 and self.index !=1 :
+            y = y[: , :, :,:-self.halo_size]
+            return y
+            # print("Rank is ", self.comm.rank, "Shape of X", x.shape, "Shape of Y", y.shape)
+        else:
+            return y
 
     def halo_exchange_forward(self, x):
         halo_region_send = x[:, :, :, -self.halo_size:]
-        if self.comm.rank == 0:
-            # chainermn.functions.send(halo_region_send, self.comm, 1)
-            self.comm.mpi_comm.send(halo_region_send, dest=self.comm.rank + 1, tag=(self.comm.rank + 1) * self.index)
-            # print("My rank is ", self.comm.rank, "sending to ", self.comm.rank + 1)
-        if self.comm.rank == 1:
-            # received_halo_region = chainermn.functions.recv(self.comm, 0)
-            received_halo_region = self.comm.mpi_comm.recv(source=self.comm.rank - 1, tag=self.comm.rank * self.index)
-            print("received halo region type" , type(received_halo_region), "size", received_halo_region.shape)
-            print("type of x", type(x), "shape", x.shape)
-            # print("received", received_halo_region.shape)
-            x = F.concat((x, received_halo_region), axis=-1)
+        if self.comm.rank < 3 :
+            chainermn.functions.send(halo_region_send, self.comm, rank=self.comm.rank + 1, tag=(self.comm.rank +1)* self.index)
+        if self.comm.rank > 0 :
+            received_halo_region = chainermn.functions.recv(self.comm, rank=self.comm.rank -1, tag=self.comm.rank*self.index)
+            x = concat((x, received_halo_region), axis=-1)
 
         return x
 
     def halo_exchange_backward(self, x):
         halo_region_send = x[:, :, :, :self.halo_size]
-        #if self.comm.rank > 0:
-            # self.comm.mpi_comm.send(halo_region_send, dest=self.comm.rank - 1, tag=(self.comm.rank - 1) * self.index * 2)
-        # if self.comm.rank < 3:
-            # received_halo_region = self.comm.mpi_comm.recv(source=self.comm.rank + 1, tag=self.comm.rank * self.index * 2)
-            # x = F.concat((received_halo_region, x), axis=-1)
+        if self.comm.rank > 0:
+            chainermn.functions.send(halo_region_send, dest=self.comm.rank - 1, tag=(self.comm.rank - 1) * self.index * 2)
+        if self.comm.rank < 3:
+            received_halo_region = self.comm.mpi_comm.recv(source=self.comm.rank + 1, tag=self.comm.rank * self.index * 2)
+            x = F.concat((received_halo_region, x), axis=-1)
 
         return x
 
