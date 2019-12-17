74e40520a6943f467345f0449e9e794799c52f4a
Albert <ankahira@gmail.com>
2019-12-17 12:46:34 +0100

    Added spatial communicator

--- chainermnx/__init__.py
+++ chainermnx/__init__.py
@@ -1,8 +1,12 @@
-
 from chainermnx import functions
 from chainermnx import links
 from chainermnx import training
 from chainermnx import utils
+from chainermnx import communicators
+from chainermnx import iterators
+from chainermnx.optimizers import create_multi_node_optimizer
+
+
 
 
 
